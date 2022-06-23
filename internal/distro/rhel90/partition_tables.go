d85aca94335daed7fa4a66fb3ed5ef96950db939
Irene Diez <7flying@users.noreply.github.com>
2022-06-23 10:26:25 +0200

    More tests

--- internal/distro/rhel90/partition_tables.go
+++ internal/distro/rhel90/partition_tables.go
@@ -219,7 +219,7 @@ var edgeBasePartitionTables = distro.BasePartitionTableMap{
 					Clevis: &disk.ClevisBind{
 						Pin:              "null",
 						Policy:           "{}",
-						RemovePassphrase: true,
+						RemovePassphrase: false,
 					},
 					Payload: &disk.LVMVolumeGroup{
 						Name:        "rootvg",
