f63eb552c6cd52c458ab728208ccea6e6fb852b4
Irene Diez <idiez@redhat.com>
2022-10-18 16:23:14 +0200

    rhel9/pipelines: add systemd_journald_stage
    Adds the `systemd-journald` stage to `ostreeDeployPipeline`
    with the persistent option set. This stores journald logs
    on disk.
    
    Relevant tests for the new stage are also added.
    
    Signed-off-by: Irene Diez <idiez@redhat.com>

---
+++ internal/osbuild/systemd_journald_stage.go
@@ -0,0 +1,56 @@
+package osbuild
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type SystemdJournaldStageOptions struct {
+	Filename string                      `json:"filename"`
+	Config   SystemdJournaldConfigDropin `json:"config"`
+}
+
+func (SystemdJournaldStageOptions) isStageOptions() {}
+
+func NewSystemdJournaldStage(options *SystemdJournaldStageOptions) *Stage {
+	return &Stage{
+		Type:    "org.osbuild.systemd-journald",
+		Options: options,
+	}
+}
+
+type SystemdJournaldConfigDropin struct {
+	Journal SystemdJournaldConfigJournalSection `json:"Journal"`
+}
+
+// 'Journal' configuration section, at least one option must be specified
+type SystemdJournaldConfigJournalSection struct {
+	// Controls where to store journal data.
+	Storage *string `json:"Storage,omitempty"`
+	// Sets whether the data objects stored in the journal should be
+	// compressed or not. Can also take threshold values.
+	Compress *string `json:"Compress,omitempty"`
+	// Splits journal files per user or to a single file.
+	SplitMode *string `json:"SplitMode,omitempty"`
+	// Max time to store entries in a single file. By default seconds, may be
+	// sufixed with units (year, month, week, day, h, m) to override this.
+	MaxFileSec *string `json:"MaxFileSec,omitempty"`
+	// Maximum time to store journal entries. By default seconds, may be sufixed
+	// with units (year, month, week, day, h, m) to override this.
+	MaxRetentionSec *string `json:"MaxRetentionSec,omitempty"`
+	// Timeout before synchronizing journal files to disk. Minimum 0.
+	SyncIntervalSec *int `json:"SyncIntervalSec,omitempty"`
+	// Enables/Disables kernel auditing on start-up, leaves it as is if
+	// unspecified.
+	Audit *string `json:"Audit,omitempty"`
+}
+
+type systemdJournaldConfigJournalSection SystemdJournaldConfigJournalSection
+
+func (s systemdJournaldConfigJournalSection) MarshalJSON() ([]byte, error) {
+	if s == (systemdJournaldConfigJournalSection{}) {
+		return nil, fmt.Errorf("at least one 'Journal' section option must be specified")
+	}
+	journalSection := systemdJournaldConfigJournalSection(s)
+	return json.Marshal(journalSection)
+}
---
+++ internal/osbuild/systemd_journald_stage_test.go
@@ -0,0 +1,58 @@
+package osbuild
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/osbuild/osbuild-composer/internal/common"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewSystemdJournalStage(t *testing.T) {
+	expectedStage := &Stage{
+		Type:    "org.osbuild.systemd-journald",
+		Options: &SystemdJournaldStageOptions{},
+	}
+	actualStage := NewSystemdJournaldStage(&SystemdJournaldStageOptions{})
+	assert.Equal(t, expectedStage, actualStage)
+}
+
+func TestSystemdJournaldStage_MarshalJSON_Invalid(t *testing.T) {
+	tests := []struct {
+		name    string
+		options SystemdJournaldStageOptions
+	}{
+		{
+			name:    "empty-options",
+			options: SystemdJournaldStageOptions{},
+		},
+		{
+			name: "no-section-options",
+			options: SystemdJournaldStageOptions{
+				Filename: "10-some-file.conf",
+				Config: SystemdJournaldConfigDropin{
+					Journal: SystemdJournaldConfigJournalSection{},
+				},
+			},
+		},
+	}
+	for idx, te := range tests {
+		t.Run(te.name, func(t *testing.T) {
+			gotBytes, err := json.Marshal(te.options)
+			assert.NotNilf(t, err, "json.Marshall() didn't return an error, but: %s [idx: %d]", string(gotBytes), idx)
+		})
+	}
+
+	testOk := SystemdJournaldStageOptions{
+		Filename: "20-another-file.conf",
+		Config: SystemdJournaldConfigDropin{
+			Journal: SystemdJournaldConfigJournalSection{
+				Storage: common.StringToPtr("persistent"),
+			},
+		},
+	}
+	assert := assert.New(t)
+	gotBytes, err := json.Marshal(testOk)
+	assert.NoError(err)
+	assert.NotEmpty(gotBytes)
+}
